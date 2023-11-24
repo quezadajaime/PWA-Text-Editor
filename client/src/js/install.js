3c9f2ee3617d85270a752df74c06e66b6fd47ccc
Jaime <quezadajaime@ymail.com>
2023-11-24 07:57:09 -0800

    worked on TODOs

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,31 @@
 const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    //stores the event
+    window.defferedPrompt = event;
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+    //removes class from the button
+    butInstall.classList.toggle("hidden", false);
+});
+
+//implements a click event handler on the "butInstall" element
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.defferedPrompt;
+
+    if (!promptEvent) {
+        return;
+    }
+    //shows prompt
+    promptEvent.promt();
+    //resets the deferred prompt variable, it can only be used once.
+    window.defferedPrompt = null;
+
+    butInstall.classList.toggle("hidden", true);
+});
+
+window.addEventListener('appinstalled', (event) => {
+    //clears prompt
+    window.defferedPrompt = null;
+});
